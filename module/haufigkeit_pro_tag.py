417f64d3f6d2cad10311def18801cb3987a030fd
Karsten Jochens <karsten.jochens@gmail.com>
2025-02-11 10:31:32 +0100

    Füge InfluxDB-Integration hinzu und implementiere tägliche Songhäufigkeitsanalyse

--- module/haufigkeit_pro_tag.py
+++ module/haufigkeit_pro_tag.py
@@ -1,5 +1,7 @@
-from collections import Counter
 import pandas as pd
+from collections import Counter
+import module.modul_config.influxdb_module as influxdb_module
+import module.modul_config.config_module as config_module
 
 def get_song_frequency_by_day(client, station_name):
     """
@@ -38,18 +40,25 @@ def print_most_frequent_songs_by_day(song_counter, station_name):
     print(f"\nHäufigste Songs für {station_name} pro Tag:")
     for (artist, title, played_date), count in song_counter.items():
         print(f"Am {played_date}: {artist} - {title}: {count} mal gespielt")
-        
+
 def run_by_day():
     """
     Lädt die Konfiguration und führt die Analyse der Songhäufigkeiten nach Datum aus.
     """
+    # Konfiguration und Stationen laden
     config = config_module.load_config()
     stations = config_module.load_stations()
 
+    # InfluxDB-Client initialisieren
     client = influxdb_module.initialize_influxdb(config)
 
+    # Für jede Station die Titel-Künstler-Häufigkeit pro Tag abrufen und ausgeben
     for station_name in stations:
         song_counter = get_song_frequency_by_day(client, station_name)
         print_most_frequent_songs_by_day(song_counter, station_name)
 
+    # InfluxDB-Client schließen
     client.close()
+
+if __name__ == "__main__":
+    run_by_day()
